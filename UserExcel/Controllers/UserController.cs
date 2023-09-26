8f44f3cd499718a3cec497733d4511d52bad73a7
Pushti Prajapati <pushti.prajapati@reynasolutions.com>
2023-09-26 10:59:52 +0530

    PP - UPDATE TABLE

--- UserExcel/Controllers/UserController.cs
+++ UserExcel/Controllers/UserController.cs
@@ -205,13 +205,13 @@ namespace UserExcel.Controllers
 
         public IActionResult ReportDownload()
         {
-            Document pdfDoc = new Document(PageSize.A4, 25, 25, 25, 15);
+            Document pdfDoc = new Document(PageSize.A3, 25, 25, 25, 15);
 
-            if (System.IO.File.Exists("D:\\CHW.pdf"))
-            {
-                System.IO.File.Delete("D:\\CHW.pdf");
-            }
-            FileStream FS = new FileStream("D:\\CHW.pdf", FileMode.Create);
+            //if (System.IO.File.Exists("D:\\CHW.pdf"))
+            //{
+            //    System.IO.File.Delete("D:\\CHW.pdf");
+            //}
+            FileStream FS = new FileStream("D:\\Summary Report.pdf", FileMode.Create);
 
             PdfWriter pdfWriter = PdfWriter.GetInstance(pdfDoc, FS);
 
@@ -219,10 +219,10 @@ namespace UserExcel.Controllers
 
             #region Title
 
-            Paragraph header = new Paragraph("Click Heal Weal", new Font(Font.FontFamily.HELVETICA, 19, Font.NORMAL));
-            header.SpacingBefore = 10f;
-            header.IndentationLeft = 50f;
-            pdfDoc.Add(header);
+            //Paragraph header = new Paragraph("Click Heal Weal", new Font(Font.FontFamily.HELVETICA, 19, Font.NORMAL));
+            //header.SpacingBefore = 10f;
+            //header.IndentationLeft = 50f;
+            //pdfDoc.Add(header);
 
             #endregion
 
@@ -234,72 +234,108 @@ namespace UserExcel.Controllers
             table.HorizontalAlignment = 20;
             table.SpacingBefore = 20f;
             table.SpacingAfter = 30f;
-
-
-            PdfPCell cell = new PdfPCell();
-
+             
             PdfPCell Title = new PdfPCell(new Phrase("Summary Report", new Font(Font.FontFamily.HELVETICA, 22, Font.BOLD)));
             Title.HorizontalAlignment = Element.ALIGN_CENTER;
-            //cell.AddElement(Title);
+            Title.VerticalAlignment = Element.ALIGN_MIDDLE;
             Title.Colspan = 3;
-            Title.HorizontalAlignment = 1;
-            Title.EnableBorderSide(10);
+            Title.FixedHeight = 40f;
             Title.BorderColor = BaseColor.BLACK;
-
-            //cell = new PdfPCell();
-            //Paragraph Title = new Paragraph("Summary Report",new Font(Font.FontFamily.HELVETICA, 22, Font.BOLD));
-            //cell.AddElement(Title);
-            //cell.Colspan = 3;
-            //cell.HorizontalAlignment = 1;
-            //cell.EnableBorderSide(10);
-            //cell.BorderColor = BaseColor.BLACK;
-           
             table.AddCell(Title);
             pdfDoc.Add(table);
+
             #endregion
 
             #region 2nd table
+
             //CREATE 2ND TABLE
             
             //  Table - Patient Information
             PdfPTable SecondTable = new PdfPTable(3);
-            SecondTable.TotalWidth = 178f;
+            SecondTable.TotalWidth = 260f;
             SecondTable.LockedWidth = true;
+            
 
-            cell = new PdfPCell();
+            PdfPCell cell = new PdfPCell();
+            
             Paragraph Header = new Paragraph("Patient Information", new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD));
             cell.AddElement(Header);
             cell.Colspan = 3;
             cell.HorizontalAlignment = 1;
             cell.BorderColor = BaseColor.BLACK;
-           
+            cell.FixedHeight = 30f;
+            cell.PaddingLeft = 6f;
+            //Border
+            cell.Border = 0;
+            cell.BorderWidthLeft = 1f;
+            cell.BorderWidthTop = 1f;
+            cell.BorderWidthRight = 1f;
             SecondTable.AddCell(cell);
 
             //create sub header
             cell = new PdfPCell();
             Paragraph subHeader = new Paragraph("First Name", new Font(Font.FontFamily.HELVETICA, 11, Font.BOLD));
+            cell.FixedHeight = 30f;
+            SecondTable.DefaultCell.Border = 0;
             cell.AddElement(subHeader);
+            cell.PaddingLeft = 6f;
+            cell.Border = 0;
+            cell.BorderWidthLeft = 1f;
             SecondTable.AddCell(cell);
 
             cell = new PdfPCell();
             subHeader = new Paragraph("Last Name", new Font(Font.FontFamily.HELVETICA, 11, Font.BOLD));
+            cell.FixedHeight = 30f;
             cell.AddElement(subHeader);
+            cell.PaddingLeft = 6f;
+            //Border
+            cell.Border = 0;
             SecondTable.AddCell(cell);
 
             cell = new PdfPCell();
             subHeader = new Paragraph("Dignosis", new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD));
+            cell.FixedHeight = 30f;
+            cell.PaddingLeft = 10f;
+            cell.Border = 0;
+            cell.BorderWidthRight = 1f;
             cell.AddElement(subHeader);
             SecondTable.AddCell(cell);
 
-            SecondTable.AddCell("jhon");
-            SecondTable.AddCell("deo");
-            SecondTable.AddCell("hypertension");
+            cell = new PdfPCell();
+            Paragraph Data = new Paragraph("jhon");
+            cell.FixedHeight = 30f;
+            cell.Border = 0;
+            cell.BorderWidthLeft = 1f;
+            cell.BorderWidthBottom = 1f;
+            cell.PaddingLeft = 16f;
+            cell.AddElement(Data);
+            SecondTable.AddCell(cell);
+
+            cell = new PdfPCell();
+            Data = new Paragraph("deo");
+            cell.FixedHeight = 30f;
+            cell.Border = 0;
+            cell.BorderWidthBottom = 1f;
+            cell.PaddingLeft = 16f;
+            cell.AddElement(Data);
+            SecondTable.AddCell(cell);
+
+            cell = new PdfPCell();
+            Data = new Paragraph("hypertension");
+            cell.FixedHeight = 30f;
+            cell.Border = 0;
+            cell.BorderWidthRight = 1f;
+            cell.BorderWidthBottom = 1f;
+            cell.PaddingLeft = 10f;
+            cell.AddElement(Data);
+            SecondTable.AddCell(cell);
+
             SecondTable.WriteSelectedRows(0, -1, pdfDoc.Left, pdfDoc.Top - 100, pdfWriter.DirectContent);
 
 
             //  Table - Physician Information
             SecondTable = new PdfPTable(1);
-            SecondTable.TotalWidth = 178f;
+            SecondTable.TotalWidth = 260f;
             SecondTable.LockedWidth = true;
 
             cell = new PdfPCell();
@@ -307,21 +343,44 @@ namespace UserExcel.Controllers
             cell.AddElement(Header);
             cell.BorderColor = BaseColor.BLACK;
             cell.HorizontalAlignment = 0;
-            
+            cell.FixedHeight = 30f;
+            //Border
+            cell.Border = 0;
+            cell.BorderWidthLeft = 1f;
+            cell.BorderWidthTop = 1f;
+            cell.BorderWidthRight = 1f;
+            cell.PaddingLeft = 6f;
             SecondTable.AddCell(cell);
 
             cell = new PdfPCell();
             subHeader = new Paragraph("Name", new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD));
             cell.AddElement(subHeader);
+            cell.FixedHeight = 30f;
+            //Border
+            cell.Border = 0;
+            cell.BorderWidthLeft = 1f;
+            cell.BorderWidthRight = 1f;
+            cell.PaddingLeft = 6f;
             SecondTable.AddCell(cell);
 
-            SecondTable.AddCell("Jhon");
-            SecondTable.WriteSelectedRows(0, -1, pdfDoc.Left + 183, pdfDoc.Top - 100, pdfWriter.DirectContent);
+            cell = new PdfPCell();
+            Data = new Paragraph("Jhon");
+            cell.FixedHeight = 30f;
+            cell.Border = 0;
+            cell.BorderWidthLeft = 1f;
+            cell.BorderWidthRight = 1f;
+            cell.BorderWidthBottom = 1f;
+            cell.PaddingLeft = 10f;
+            cell.AddElement(Data);
+            SecondTable.AddCell(cell);
+
+            //SecondTable.AddCell("Jhon");
+            SecondTable.WriteSelectedRows(0, -1, pdfDoc.Left + 266, pdfDoc.Top - 100, pdfWriter.DirectContent);
 
 
             //  Table - Appointment Information
             SecondTable = new PdfPTable(3);
-            SecondTable.TotalWidth = 178f;
+            SecondTable.TotalWidth = 260f;
             SecondTable.LockedWidth = true;
 
             cell = new PdfPCell();
@@ -330,72 +389,101 @@ namespace UserExcel.Controllers
             cell.Colspan = 3;
             cell.BorderColor = BaseColor.BLACK;
             cell.HorizontalAlignment = 2;
-        
+            cell.PaddingLeft = 6f;
+            cell.FixedHeight = 30f;
+            //Border
+            cell.Border = 0;
+            cell.BorderWidthLeft = 1f;
+            cell.BorderWidthTop = 1f;
+            cell.BorderWidthRight = 1f;
             SecondTable.AddCell(cell);
 
             cell = new PdfPCell();
             subHeader = new Paragraph("Date", new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD));
             cell.AddElement(subHeader);
+            cell.FixedHeight = 30f;
+            cell.Border = 0;
+            cell.BorderWidthLeft = 1f;
+            cell.PaddingLeft = 16f;
             SecondTable.AddCell(cell);
 
             cell = new PdfPCell();
             subHeader = new Paragraph("Time", new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD));
             cell.AddElement(subHeader);
+            cell.PaddingLeft = 16f;
+            cell.FixedHeight = 30f;
+            cell.Border = 0;
             SecondTable.AddCell(cell);
 
             cell = new PdfPCell();
             subHeader = new Paragraph("Loction", new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD));
+            cell.FixedHeight = 30f;
+            cell.PaddingLeft = 16f;
+            cell.Border = 0;
+            cell.BorderWidthRight = 1f;
             cell.AddElement(subHeader);
-            
+            SecondTable.AddCell(cell);
+
+     
+            cell = new PdfPCell();
+            Data = new Paragraph("28 / 07 / 2022");
+            cell.FixedHeight = 30f;
+            cell.Border = 0;
+            cell.BorderWidthLeft = 1f;
+            cell.BorderWidthBottom = 1f;
+            cell.PaddingLeft = 6f;
+            cell.AddElement(Data);
+            SecondTable.AddCell(cell);
+
+
+            cell = new PdfPCell();
+            Data = new Paragraph("01:00");
+            cell.FixedHeight = 30f;
+            cell.Border = 0;
+            cell.BorderWidthBottom = 1f;
+            cell.PaddingLeft = 16f;
+            cell.AddElement(Data);
             SecondTable.AddCell(cell);
 
             cell = new PdfPCell();
-            Paragraph data = new Paragraph("28 / 07 / 2022");
-            cell.AddElement(data);
-            
+            Data = new Paragraph("Vadodara");
+            cell.AddElement(Data);
+            cell.FixedHeight = 30f;
+            cell.Border = 0;
+            cell.BorderWidthRight = 1f;
+            cell.BorderWidthBottom = 1f;
+            cell.PaddingLeft = 16f;
             SecondTable.AddCell(cell);
-
-            cell = new PdfPCell();
-            data = new Paragraph("01:00");
-            cell.AddElement(data);
             
-            SecondTable.AddCell(cell);
-
-            cell = new PdfPCell();
-            data = new Paragraph("Vadodara");
-            cell.AddElement(data);
-            
-            SecondTable.AddCell(cell);
-
-            
-            SecondTable.WriteSelectedRows(0, -1, pdfDoc.Left + 366, pdfDoc.Top - 100, pdfWriter.DirectContent);
+            SecondTable.WriteSelectedRows(0, -1, pdfDoc.Left + 532, pdfDoc.Top - 100, pdfWriter.DirectContent);
+           // pdfDoc.Add(SecondTable);
 
             #endregion
 
             #region 3rd Table
 
 
-            PdfPTable UserTable = new PdfPTable(3);
-            UserTable.WidthPercentage = 100;
-            UserTable.HorizontalAlignment = 20;
-            UserTable.SpacingBefore = 100f;
-            UserTable.SpacingAfter = 30f;
+            //PdfPTable UserTable = new PdfPTable(3);
+            //UserTable.WidthPercentage = 100;
+            //UserTable.HorizontalAlignment = 20;
+            //UserTable.SpacingBefore = 150f;
+            //UserTable.SpacingAfter = 30f;
            
 
-            cell = new PdfPCell();
-            Paragraph UserHeader = new Paragraph("First Name", new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD));
-            cell.AddElement(UserHeader);
-            UserTable.AddCell(cell);
+            //cell = new PdfPCell();
+            //Paragraph UserHeader = new Paragraph("First Name", new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD));
+            //cell.AddElement(UserHeader);
+            //UserTable.AddCell(cell);
 
-            cell = new PdfPCell();
-            UserHeader = new Paragraph("Middle Name", new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD));
-            cell.AddElement(UserHeader);
-            UserTable.AddCell(cell);
+            //cell = new PdfPCell();
+            //UserHeader = new Paragraph("Middle Name", new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD));
+            //cell.AddElement(UserHeader);
+            //UserTable.AddCell(cell);
 
-            cell = new PdfPCell();
-            UserHeader = new Paragraph("Last Name", new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD));
-            cell.AddElement(UserHeader);
-            UserTable.AddCell(cell);
+            //cell = new PdfPCell();
+            //UserHeader = new Paragraph("Last Name", new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD));
+            //cell.AddElement(UserHeader);
+            //UserTable.AddCell(cell);
             
 
             string connect = "Server=ARCHE-ITD440\\SQLEXPRESS;Database=UserDB;Trusted_Connection=True;TrustServerCertificate=True;";
@@ -410,14 +498,97 @@ namespace UserExcel.Controllers
                 {
                     while (rdr.Read())
                     {
-                        UserTable.AddCell(rdr[0].ToString());
-                        UserTable.AddCell(rdr[1].ToString());
-                        UserTable.AddCell(rdr[2].ToString());
-                    }
-                  
+                        PdfPTable UserTable = new PdfPTable(3);
+                        UserTable.TotalWidth = 260f;
+                        UserTable.LockedWidth = true;                     
+                        UserTable.SpacingBefore = 90f;
+                          
+                        cell = new PdfPCell();
+                        Header = new Paragraph("User Detail", new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD));
+                        cell.AddElement(Header);
+                        cell.Colspan = 3;
+                        cell.BorderColor = BaseColor.BLACK;
+                        cell.HorizontalAlignment = 0;
+                        cell.PaddingLeft = 6f;
+                        cell.FixedHeight = 30f;
+                        //Border
+                        cell.Border = 0;
+                        cell.BorderWidthLeft = 1f;
+                        cell.BorderWidthTop = 1f;
+                        cell.BorderWidthRight = 1f;
+                        UserTable.AddCell(cell);
+
+                        cell = new PdfPCell();
+                        subHeader = new Paragraph("First Name", new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD));
+                        cell.AddElement(subHeader);
+                        cell.PaddingLeft = 6f;
+                        cell.FixedHeight = 30f;
+                        //Border
+                        cell.Border = 0;
+                        cell.BorderWidthLeft = 1f;
+                       
+                        UserTable.AddCell(cell);
+
+                        cell = new PdfPCell();
+                        subHeader = new Paragraph("Middle Name", new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD));
+                        cell.AddElement(subHeader);
+                        cell.PaddingLeft = 6f;
+                        cell.FixedHeight = 30f;
+                        //Border
+                        cell.Border = 0;
+                        UserTable.AddCell(cell);
+
+                        cell = new PdfPCell();
+                        subHeader = new Paragraph("Last Name", new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD));
+                        cell.AddElement(subHeader);
+                        cell.PaddingLeft = 6f;
+                        cell.FixedHeight = 30f;
+                        //Border
+                        cell.Border = 0;
+                        cell.BorderWidthRight = 1f;
+                        UserTable.AddCell(cell);
+
+                        cell = new PdfPCell();
+                        subHeader = new Paragraph(rdr[0].ToString());
+                        cell.AddElement(subHeader);
+                        cell.PaddingLeft = 16f;
+                        cell.FixedHeight = 30f;
+                        //Border
+                        cell.Border = 0;
+                        cell.BorderWidthLeft = 1f;
+                        cell.BorderWidthBottom = 1f;
+                        UserTable.AddCell(cell);
+
+                        cell = new PdfPCell();
+                        subHeader = new Paragraph(rdr[1].ToString());
+                        cell.AddElement(subHeader);
+                        cell.PaddingLeft = 16f;
+                        cell.FixedHeight = 30f;
+                        //Border
+                        cell.Border = 0;
+                        cell.BorderWidthBottom = 1f;
+                        UserTable.AddCell(cell);
+
+                        cell = new PdfPCell();
+                        subHeader = new Paragraph(rdr[2].ToString());
+                        cell.AddElement(subHeader);
+                        cell.PaddingLeft = 16f;
+                        cell.FixedHeight = 30f;
+                        //Border
+                        cell.Border = 0;
+                        cell.BorderWidthRight = 1f;
+                        cell.BorderWidthBottom = 1f;
+                        UserTable.AddCell(cell);
+
+
+                        // UserTable.AddCell(rdr[0].ToString());
+                        // UserTable.AddCell(rdr[1].ToString());
+                        // UserTable.AddCell(rdr[2].ToString());
+
+                       // UserTable.WriteSelectedRows(0, -1, pdfDoc.Left , pdfDoc.Top - 200, pdfWriter.DirectContent);
+                        pdfDoc.Add(UserTable);
+                    } 
                 }
-                pdfDoc.Add(UserTable);
-       
             }
 
             #endregion
